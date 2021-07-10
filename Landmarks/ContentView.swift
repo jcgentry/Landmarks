dd4fbbb80100afc6bb1823ccb735ce927728c230
gentj <Jamie.Gentry@pega.com>
2021-07-10 19:44:36 -0400

    Complete layout

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,18 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
+            MapView()
+                .frame(height: 300.0)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .offset(y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
+                .padding(/*@START_MENU_TOKEN@*/.bottom, -130.0/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                                   .font(.title2)
+                               Text("Descriptive text goes here.")
 
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
