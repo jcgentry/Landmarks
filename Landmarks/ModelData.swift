8e39027ae413f4ccfe422d5a6c1bedf1637c0e49
gentj <Jamie.Gentry@pega.com>
2021-07-11 10:44:04 -0400

    Start working on data

---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Landmarks
+//
+//  Created by Gentry, Jamie on 7/11/21.
+//
+
+import Foundation
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    
+}
