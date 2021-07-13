8db7a48f6148af4020b3a2e19918ac7beadfcd76
gentj <Jamie.Gentry@pega.com>
2021-07-13 17:12:24 -0400

    Add favorite star

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -18,6 +18,11 @@ struct LandmarkRow: View {
             Text(landmark.name)
             
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+            }
         }
     }
 }
